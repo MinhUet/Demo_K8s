3803bfa275b29f88900e7250cf8eb3c7cab40ec6
MinhUet <buivanminh1309@gmail.com>
2020-12-09 22:15:32 +0700

    k8s

---
+++ Nhom9.Docker_Kubernet.Demo.DL/Database/MongoDatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Nhom9.Docker_Kubernet.Demo.Entity.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nhom9.Docker_Kubernet.Demo.DL.Database
+{
+    public class MongoDatabaseContext : IDatabaseContext
+    {
+        private readonly IMongoDatabase _mongoDatabase;
+        private readonly IMongoClient _mongoClient;
+        private readonly ILogger<MongoDatabaseContext> _logger;
+
+        public MongoDatabaseContext(ILogger<MongoDatabaseContext> logger, IDatabaseSettings settings)
+        {
+            _logger = logger;
+            try
+            {
+                _mongoClient = new MongoClient(/*settings.ConnectionString*/"mongodb://root:11111111@104.248.149.21:31029/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false");
+                _mongoDatabase = _mongoClient.GetDatabase(/*settings.DatabaseName*/"Demo_Docker_Kubernet");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"\nError while connecting to MongoDB: {ex}\n");
+            }
+        }
+
+        public IMongoDatabase MongoDatabase { get => _mongoDatabase; }
+    }
+}
